219eda677edcbabf500b85a8e7d459b1252f4684
sojeong <th21@naver.com>
2025-06-14 15:25:15 +0900

    bug: core test

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar build/libs/todays-kbo-backend-0.0.1-SNAPSHOT.jar
--- src/main/java/com/kbo/todayskbo/config/WebConfig.java
+++ src/main/java/com/kbo/todayskbo/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.kbo.todayskbo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -12,5 +14,19 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*") // 또는 정확한 도메인: "http://localhost:8081"
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("https://port-0-todays-kbo-backend-mbvuay6v1dd37179.sel4.cloudtype.app")
+                // 이 설정은 https://sub.example.app 인 origin을 허용합니다.
+                ;
+            }
+        };
     }
 }
